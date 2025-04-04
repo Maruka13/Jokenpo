595fc6209dfdf3be27c19d9e195c209a616596dd
Maruka <emanuelle_carollina@hotmail.com>
2025-04-03 22:04:15 -0300

    verif idade

---
+++ script.js
@@ -0,0 +1,12 @@
+function jogar() {
+    idade = prompt(Quantos anos você tem?)
+    if (idade < 18) {
+        alert ("Você não pode jogar este jogo.")
+    } 
+
+    if (idade >= 18) {
+        escolhaJogador = 
+        escolhaComputador = 
+    }
+
+}
